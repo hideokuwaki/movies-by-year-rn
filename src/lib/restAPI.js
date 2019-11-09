3fef242d11ce84ded63facfa36b1e76dbc9f5576
Hideo <hideokuwaki@users.noreply.github.com>
2019-11-09 18:21:50 -0300

    domain configuration

--- src/lib/restAPI.js
+++ src/lib/restAPI.js
@@ -1,9 +1,12 @@
 import { ToastAndroid } from 'react-native';
 import axios from 'axios';
 
+//const DOMAIN = 'http://moviesbyyear.us-east-2.elasticbeanstalk.com';
+const DOMAIN = 'http://192.168.15.5:3000';
+
 export const getMoviesByYear = async (name = '') => {
 
-  const url = 'http://192.168.15.5:3000/api/movies/count';
+  const url = DOMAIN + '/api/movies/count';
 
   let res = {};
 
